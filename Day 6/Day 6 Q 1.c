69d01268259f6a745105a3af983c151c7924de57
Ajitsingh Devda <130886882+Ajit745@users.noreply.github.com>
2024-07-01 11:19:51 +0530

    Add files via upload

---
+++ Day 6/Day 6 Q 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y;
+	printf("Enter First Number :");
+	scanf("%d",&x);
+	printf("Enter Second Number :");
+	scanf("%d",&y);
+	(x<y)?printf("x is mininum"):printf("y is minimum");
+}
