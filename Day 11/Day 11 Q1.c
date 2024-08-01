b563a25bbc39cf62d3edf32ae88ba80e8355fd12
Ajitsingh Devda <130886882+Ajit745@users.noreply.github.com>
2024-08-01 11:16:22 +0530

    Add files via upload

---
+++ Day 11/Day 11 Q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	//Write a Program to print 1 to 10 using a do-while loop.
+	int i=1;
+	do
+	{
+		printf("%d ",i);
+		i++;
+	}while(i<=10);
+}
---
+++ Day 11/Day 11 Q2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	//Write a Program to print 10 to 1 using a for loop.
+	int i=10;
+	for(i=10; i>=1; i--)
+	{
+		printf("%d ",i);
+	}
+}
