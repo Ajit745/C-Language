bbaa26ed8ca5cbf1aeba273e7357e9edc5a577bc
Ajitsingh Devda <130886882+Ajit745@users.noreply.github.com>
2024-08-23 11:41:42 +0530

    Add files via upload

---
+++ Day 15 2d Array/Q 2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void main()
+{
+	int r,c,i,j;
+	printf("Enter The Number Of Rows For The Array :");
+	scanf("%d",&r);
+	printf("Enter The Number Of Columns For The Array :");
+	scanf("%d",&c);
+	
+	int a[r][c];
+	printf("Enter The Value of Array :\n");
+	
+	for(i=0; i<r; i++) 
+	{
+		for(j=0; j<c; j++)
+		{
+			printf("a[%d][%d] : ",i,j);
+			scanf("%d",&a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+	for(i=0; i<r; i++) 
+	{ 
+		for(j=0; j<c; j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+		printf("\n");	
+	printf("The Length Of Array is %d ",r*c);
+}
